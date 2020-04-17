a5801df9f0394f13b8488471c73a32078cd9df25
Erik Toor <eriktoor@gmail.com>
2020-04-17 03:23:34 -0400

    Create globals.py

---
+++ globals.py
@@ -0,0 +1,10 @@
+# personal details 
+# Obtain Keys under 'Keys and Tokens'at https://developer.twitter.com/en/apps/[APP #] 
+api_key = "APIKEY"
+api_secret = "APISECRET"
+access_token =" "ACCESSSTOKEN"
+access_token_secret = "ACCESSTOKENSECRET"
+
+account_id = "1250155034232475650"
+account_name = "CovidAsk"
+error_message = "Sorry I don't understand your tweet. Please try putting in a specific city or county you would like #COVID19 updates for!" 
