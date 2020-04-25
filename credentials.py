432620542ce4bd21403b2c347df3e70013db0f9f
Erik Toor <eriktoor@gmail.com>
2020-04-25 02:07:34 -0400

    Update and rename globals.py to credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# personal details 
+# Obtain Keys under 'Keys and Tokens'at https://developer.twitter.com/en/apps/[APP #] 
+API_KEY ="API_KEY" 
+API_SECRET ="API_SECRET" 
+ACCESS_TOKEN ="ACCESS_TOKEN"
+ACCESS_TOKEN_SECRET ="ACCESS_TOKEN_SECRET"
+
+ACCOUNT_ID = "1250155034232475650"
+ACCOUNT_NAME = "CovidAsk"
